e1a596e5c72aedb0ac829059d56f94a597545195
platypus46 <svolts2185@gmail.com>
2023-11-18 09:32:24 +0900

    orbit test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
-import React, { Suspense, useEffect } from 'react';
-import { Canvas, useThree } from '@react-three/fiber';
+import React, { Suspense, useEffect, useRef } from 'react';
+import { Canvas, useThree,  useFrame  } from '@react-three/fiber';
 import { Environment, OrbitControls, useGLTF } from '@react-three/drei';
-import { DirectionalLight as ThreeDirectionalLight } from 'three';
+import { DirectionalLight as ThreeDirectionalLight,Object3D } from 'three';
 
 interface DirectionalLightProps {
   position: [number, number, number];
@@ -9,57 +9,144 @@ interface DirectionalLightProps {
   color: string;
 }
 
+
+const earthPosition = { x: 0, y: 0, z: 0 };
+
+interface PlanetModelProps {
+  modelPath: string;
+  orbitRadius: number;
+  orbitSpeed: number;
+  isEarth?: boolean; 
+}
+
+function PlanetFunc({ modelPath, orbitRadius, orbitSpeed, isEarth = false }: PlanetModelProps) {
+  const planetRef = useRef<Object3D>(null);
+  const gltf = useGLTF(modelPath);
+
+  useFrame(({ clock }) => {
+    const elapsedTime = clock.getElapsedTime();
+    const angle = elapsedTime * orbitSpeed;
+
+    if (planetRef.current) {
+      const x = orbitRadius * Math.cos(angle);
+      const z = orbitRadius * Math.sin(angle);
+
+      planetRef.current.position.set(x, 0, z);
+
+      if (isEarth) {
+        earthPosition.x = x;
+        earthPosition.z = z;
+      }
+    }
+  });
+
+  return <primitive ref={planetRef} object={gltf.scene} />;
+}
+
+
 function SunModel() {
   const gltf = useGLTF('/images/SolarSystemElements/sun.gltf');
   return <primitive object={gltf.scene} />;
 }
 
 function MercuryModel() {
-  const gltf = useGLTF('/images/SolarSystemElements/mercury.gltf');
-
-  return <primitive object={gltf.scene} position={[100, 0, 0]} />;
+  return (
+    <PlanetFunc
+      modelPath="/images/SolarSystemElements/mercury.gltf"
+      orbitRadius={100}
+      orbitSpeed={4.15} 
+    />
+  );
 }
 
 function VenusModel() {
-  const gltf = useGLTF('/images/SolarSystemElements/venus.gltf');
-
-  return <primitive object={gltf.scene} position={[200, 0, 0]} />;
+  return (
+    <PlanetFunc
+      modelPath="/images/SolarSystemElements/venus.gltf"
+      orbitRadius={200}
+      orbitSpeed={1.62} 
+    />
+  );
 }
+
 function EarthModel() {
-  const gltf = useGLTF('/images/SolarSystemElements/earth.gltf');
-
-  return <primitive object={gltf.scene} position={[300, 0, 0]} />;
+  return (
+    <PlanetFunc
+      modelPath="/images/SolarSystemElements/earth.gltf"
+      orbitRadius={300}
+      orbitSpeed={1.0}
+      isEarth={true}
+    />
+  );
 }
+
 function MoonModel() {
+  const moonRef = useRef<Object3D>(null);
   const gltf = useGLTF('/images/SolarSystemElements/moon.gltf');
 
-  return <primitive object={gltf.scene} position={[400, 0, 0]} />;
+  useFrame(({ clock }) => {
+    const elapsedTime = clock.getElapsedTime();
+    const angle = elapsedTime * 13.38;
+
+    if (moonRef.current) {
+      const x = earthPosition.x + (20 * Math.cos(angle));
+      const z = earthPosition.z + (20 * Math.sin(angle));
+      moonRef.current.position.set(x, 0, z);
+    }
+  });
+
+  return <primitive ref={moonRef} object={gltf.scene} />;
 }
+
+
 function MarsModel() {
-  const gltf = useGLTF('/images/SolarSystemElements/mars.gltf');
-
-  return <primitive object={gltf.scene} position={[500, 0, 0]} />;
+  return (
+    <PlanetFunc
+      modelPath="/images/SolarSystemElements/mars.gltf"
+      orbitRadius={500}
+      orbitSpeed={0.53} 
+    />
+  );
 }
+
 function JupiterModel() {
-  const gltf = useGLTF('/images/SolarSystemElements/jupiter.gltf');
-
-  return <primitive object={gltf.scene} position={[600, 0, 0]} />;
+  return (
+    <PlanetFunc
+      modelPath="/images/SolarSystemElements/jupiter.gltf"
+      orbitRadius={600}
+      orbitSpeed={0.084}
+    />
+  );
 }
+
 function SaturnModel() {
-  const gltf = useGLTF('/images/SolarSystemElements/saturn.gltf');
-
-  return <primitive object={gltf.scene} position={[700, 0, 0]} />;
+  return (
+    <PlanetFunc
+      modelPath="/images/SolarSystemElements/saturn.gltf"
+      orbitRadius={700}
+      orbitSpeed={0.034} 
+    />
+  );
 }
-function UranusModel() {
-  const gltf = useGLTF('/images/SolarSystemElements/uranus.gltf');
 
-  return <primitive object={gltf.scene} position={[800, 0, 0]} />;
+function UranusModel() {
+  return (
+    <PlanetFunc
+      modelPath="/images/SolarSystemElements/uranus.gltf"
+      orbitRadius={800}
+      orbitSpeed={0.012} 
+    />
+  );
 }
 
 function NeptuneModel() {
-  const gltf = useGLTF('/images/SolarSystemElements/neptune.gltf');
-
-  return <primitive object={gltf.scene} position={[900, 0, 0]} />;
+  return (
+    <PlanetFunc
+      modelPath="/images/SolarSystemElements/neptune.gltf"
+      orbitRadius={900}
+      orbitSpeed={0.006} 
+    />
+  );
 }
 
 
@@ -82,7 +169,7 @@ function DirectionalLight(props: DirectionalLightProps) {
 
 export default function Home() {
   return (
-    <div className="bg-cover bg-center h-screen" style={{ backgroundImage: `url('/images/background.png')` }}>
+    <div className="bg-cover bg-center h-screen" style={{ backgroundImage: `url('/images/sky.jpg')` }}>
       <Canvas>
         <Suspense fallback={null}>
           <SunModel />
@@ -99,7 +186,7 @@ export default function Home() {
   
           <DirectionalLight 
             position={[0, 0, 0]} 
-            intensity={10} 
+            intensity={50} 
             color="white" 
           />
 
